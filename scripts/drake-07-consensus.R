cd2e959d174a1fce85e0f203778bcdc8e31c3f6f
brendanf <brendan.furneaux@gmail.com>
2020-02-16 12:30:29 +0100

    Clean up consensus script.

--- scripts/drake-07-consensus.R
+++ scripts/drake-07-consensus.R
@@ -3,7 +3,7 @@ if (exists("snakemake")) {
   load(snakemake@input[["drakedata"]])
   od <- readLines(snakemake@input$flag)
 } else {
-  load("drake.Rdata")
+  load("data/plan/drake.Rdata")
 
 }
 
@@ -30,30 +30,24 @@ if (length(targets) > 0) {
   parallelism <- if (jobs > 1) "clustermq" else "loop"
   flog.info("Building %d consensus and taxonomy targets with %d jobs of %d cores...",
             length(targets), jobs, ncpus)
+  flog.info("Making targets %s...", paste(targets, collapse = "; "))
   tictoc::tic()
-  dconfig <- drake::drake_config(plan,
-       parallelism = parallelism,
-       jobs_preprocess = local_cpus(),
-       jobs = jobs,
-       retries = 1,
-       elapsed = 3600*48, #48 hours
-       keep_going = FALSE,
-       caching = "worker",
-       cache_log_file = TRUE,
-       targets = targets,
-       console_log_file = get_logfile("consensus")
+  drake::make(
+    plan,
+    parallelism = parallelism,
+    jobs_preprocess = local_cpus(),
+    jobs = jobs,
+    retries = 1,
+    elapsed = 3600*48, #48 hours
+    keep_going = FALSE,
+    caching = "worker",
+    cache_log_file = TRUE,
+    targets = targets,
+    memory_strategy = "preclean",
+    console_log_file = get_logfile("consensus")
   )
   tictoc::toc()
-  flog.info("Finding outdated targets...")
-  tictoc::tic()
-  od <- subset_outdated(targets, dconfig)
-  tictoc::toc()
-  flog.info("Found %d outdated targets.", length(od))
-  flog.info("Making targets %s...", paste(od, collapse = "; "))
-  tictoc::tic()
-  drake::make(config = dconfig)
-  tictoc::toc()
-  if (any(od %in% drake::failed())) {
+  if (any(targets %in% drake::failed())) {
     if (interactive()) stop() else quit(status = 1)
   }
   flog.info("Recalculating outdated targets...")
