c0e2a2af3ed2b393412c17b372074a20575d5043
brendanf <brendan.furneaux@gmail.com>
2020-09-26 00:32:49 +0200

    move reverselog_trans to output functions

--- scripts/output_functions.R
+++ scripts/output_functions.R
@@ -468,3 +468,11 @@ scale_color_read <- function(...) {
     ...
   )
 }
+
+reverselog_trans <- function(base = exp(1)) {
+  trans <- function(x) -log(x, base)
+  inv <- function(x) base^(-x)
+  scales::trans_new(paste0("reverselog-", format(base)), trans, inv, 
+            scales::log_breaks(base = base), 
+            domain = c(1e-100, Inf))
+}
